2d699eb5505eee1bbb4a5001c4d3b4bf05d0c6c4
slotindima@gmail.com <slotindima@gmail.com>
2018-10-29 23:07:08 +0200

    Change theme name slotin-theme -> SlotinTheme

--- app/design/frontend/Slotin/SlotinTheme/registration.php
+++ app/design/frontend/Slotin/SlotinTheme/registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/slotin/slotin-theme',
+    'frontend/Slotin/SlotinTheme',
     __DIR__
 );
