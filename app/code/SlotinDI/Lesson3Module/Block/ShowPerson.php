8fffda0d3b28a9dee9b0a77787e58528225f85f9
Slotin Dmytro <slotindima@gmail.com>
2018-11-01 15:43:46 +0200

    Add checker in showperson if variable is set. Add function for generate link to JSON

--- app/code/SlotinDI/Lesson3Module/Block/ShowPerson.php
+++ app/code/SlotinDI/Lesson3Module/Block/ShowPerson.php
@@ -4,6 +4,14 @@ namespace SlotinDI\Lesson3Module\Block;
 
 class ShowPerson extends \Magento\Framework\View\Element\Template
 {
+    const JSON_RESPONSE = 'slotindi_lesson3module/demonstration/indexjson';
+    /**
+     * @return string
+     */
+    public function getLinkToJson()
+    {
+        return $this->getUrl(self::JSON_RESPONSE);
+    }
     public function execute()
     {
         $this->_view->renderLayout();
--- app/code/SlotinDI/Lesson3Module/Controller/Demonstration/IndexJson.php
+++ app/code/SlotinDI/Lesson3Module/Controller/Demonstration/IndexJson.php
@@ -3,6 +3,7 @@ namespace SlotinDI\Lesson3Module\Controller\Demonstration;
 use Magento\Framework\Controller\ResultFactory;
 class IndexJson extends \Magento\Framework\App\Action\Action
 {
+
     /**
      * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface
      */
