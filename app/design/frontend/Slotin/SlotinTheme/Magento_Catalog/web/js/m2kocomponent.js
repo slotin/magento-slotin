82b71f64532e921b59ee00610e5f1a07d6044e40
Slotin Dmytro <slotindima@gmail.com>
2018-12-04 23:18:44 +0200

    Lesson 7 knockOut Catalog timer

---
+++ app/design/frontend/Slotin/SlotinTheme/Magento_Catalog/web/js/m2kocomponent.js
@@ -0,0 +1,42 @@
+define(
+    [
+        'jquery',
+        'uiComponent',
+        'ko',
+        'Magento_Catalog/js/model/rgb-model'
+    ],
+    function ($, Component, ko, rgbModel) {
+        'use strict';
+
+        var self;
+        return Component.extend({
+            myTimer: ko.observable(0),
+            randomColour: ko.computed(function() {
+                //we are using the aliased rgbModel here giving us access to the RGB values
+                // console.log('rgb(' + rgbModel.red() + ', ' + rgbModel.blue() + ', ' + rgbModel.green() + ')');
+                return 'rgb(' + rgbModel.red() + ', ' + rgbModel.blue() + ', ' + rgbModel.green() + ')';
+            }, this),
+            initialize: function () {
+                self = this;
+                this._super();
+                //call the incrementTime function to run on intialize
+                this.incrementTime();
+                this.subscribeToTime();
+            },
+            //increment myTimer every second
+            incrementTime: function() {
+                var t = 0;
+                setInterval(function() {
+                    t++;
+                    self.myTimer(t);
+                    // console.log(t);
+                }, 1000);
+            },
+            subscribeToTime: function() {
+                this.myTimer.subscribe(function() {
+                    rgbModel.updateColour();
+                });
+            }
+        });
+    }
+);
