f365ba665c8fcce511e0e4db5763ce2d36e5ac3c
Slotin Dmytro <slotindima@gmail.com>
2019-01-14 22:19:02 +0200

    Fix widget

--- app/code/SlotinDI/CustomWidget/Block/Widget/HomepageBanner.php
+++ app/code/SlotinDI/CustomWidget/Block/Widget/HomepageBanner.php
@@ -4,5 +4,8 @@ namespace SlotinDI\CustomWidget\Block\Widget;
 use Magento\Framework\View\Element\Template;
 use Magento\Widget\Block\BlockInterface;
 class HomepageBanner extends Template implements BlockInterface {
-    protected $_template = "widget/homepage_banner.phtml";
+    /**
+     * @var string
+     */
+    protected $_template = 'widget/homepage_banner.phtml';
 }
